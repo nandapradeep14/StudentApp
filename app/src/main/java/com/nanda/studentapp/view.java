db1509045f288410533df31b6a34c4e212090dc8
nandapradeep14 <nandapradeeppayeri@gmail.com>
2024-07-01 09:36:00 +0530

    updated

--- app/src/main/java/com/nanda/studentapp/view.java
+++ app/src/main/java/com/nanda/studentapp/view.java
@@ -1,20 +1,31 @@
 package com.nanda.studentapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class view extends AppCompatActivity {
+    AppCompatButton b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_view);
+        b3=(AppCompatButton) findViewById(R.id.view);
+/*b3.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), "viewed", Toast.LENGTH_LONG).show();
+    }
+});*/
 
     }
 }
